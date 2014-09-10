d77209f2d6016748f583ba9884bd7e97f94eeee4
Ned Ruggeri <ruggeri@self-loop.com>
2014-09-09 22:16:55 -0700

    write new TTT skeleton

---
+++ skeleton/js/ttt-widget.js
@@ -0,0 +1,20 @@
+(function () {
+  if (typeof TTT === "undefined") {
+    window.TTT = {};
+  }
+
+  var Widget = TTT.Widget = function (game, $el) {
+  };
+
+  Widget.prototype.bindEvents = function () {
+  };
+
+  Widget.prototype.makeMove = function ($square) {
+  };
+
+  Widget.prototype.play = function () {
+  };
+
+  Widget.prototype.setupBoard = function () {
+  };
+})();
