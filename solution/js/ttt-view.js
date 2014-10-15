cd7b8c733bf7eafdb684720d60289be005fd8ec0
Jonathan Tamboer <jonathanlemuel@me.com>
2014-10-15 18:02:24 -0400

    Add semantic styling.

--- solution/js/ttt-view.js
+++ solution/js/ttt-view.js
@@ -13,7 +13,7 @@
 
   View.prototype.bindEvents = function () {
     // install a handler on the `li` elements inside the board.
-    this.$el.on('click', 'div.cell', (function (event) {
+    this.$el.on("click", "li", (function (event) {
       var $square = $(event.currentTarget);
       this.makeMove($square);
     }).bind(this));
@@ -26,37 +26,44 @@
     try {
       this.game.playMove(pos);
     } catch (e) {
-      alert('Invalid move! Try again.');
+      alert("Invalid move! Try again.");
       return;
     }
 
-    $square.addClass("player-" + currentPlayer);
+    $square.addClass(currentPlayer);
 
     if (this.game.isOver()) {
       // cleanup click handlers.
-      this.$el.off('click');
+      this.$el.off("click");
+      this.$el.addClass("game-over");
 
       var winner = this.game.winner();
+      var $figcaption = $("<figcaption>");
+
       if (winner) {
-        this.$el.addClass('winner-' + winner);
+        this.$el.addClass("winner-" + winner);
+        $figcaption.html("You win, " + winner + "!");
       } else {
-        this.$el.addClass('draw');
+        $figcaption.html("It's a draw!");
       }
+
+      this.$el.append($figcaption);
     }
   };
 
   View.prototype.setupBoard = function () {
-    for (var rowIdx = 0; rowIdx < 3; rowIdx++) {
-      var $row = $('<div class="row"></div>');
+    var $ul = $("<ul>");
+    $ul.addClass("group");
 
+    for (var rowIdx = 0; rowIdx < 3; rowIdx++) {
       for (var colIdx = 0; colIdx < 3; colIdx++) {
-        var $li = $('<div class="cell">');
+        var $li = $("<li>");
         $li.data("pos", [rowIdx, colIdx]);
 
-        $row.append($li);
+        $ul.append($li);
       }
-
-      this.$el.find(".board").append($row);
     }
+
+    this.$el.append($ul);
   };
 })();
